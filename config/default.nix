0b4f4a2d39fc99cdfd609748a87372c4b899a9d4
ElrohirGT <45268815+ElrohirGT@users.noreply.github.com>
2024-02-27 14:50:28 +0000

    feat: Added Treesitter completion source

--- config/default.nix
+++ config/default.nix
@@ -134,12 +134,14 @@
           {name = "nvim_lsp";}
           {name = "path";}
           {name = "buffer";}
+          {name = "treesitter";}
         ];
       };
       cmp-nvim-lsp.enable = true;
       cmp-nvim-lsp-signature-help.enable = true;
       cmp-path.enable = true;
       cmp-buffer.enable = true;
+      cmp-treesitter.enable = true;
     };
 
     keymaps = [
