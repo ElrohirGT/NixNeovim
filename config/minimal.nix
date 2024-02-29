e6ba88cab6368d42f823c16de496430f9595dd54
ElrohirGT <elrohirgt@gmail.com>
2024-02-29 14:09:49 -0600

    feat: Reorganized everything

--- config/minimal.nix
+++ config/minimal.nix
@@ -2,10 +2,10 @@
   # Import all your configuration modules here
   imports = [
 		./general.nix
-    ./telescope.nix
-    ./completions.nix
-    ./treesitter.nix
-		./language_servers.nix
-		./mappings.nix
+    ./modules/telescope.nix
+    ./modules/completions.nix
+    ./modules/treesitter.nix
+		./modules/language_servers.nix
+		./modules/mappings.nix
   ];
 }
