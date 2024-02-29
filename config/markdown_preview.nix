a6aab98179dd574dd12e12babfa777c202709df8
ElrohirGT <elrohirgt@gmail.com>
2024-02-28 21:06:51 -0600

    feat: Added markdown preview keymaps

--- config/markdown_preview.nix
+++ config/markdown_preview.nix
@@ -10,4 +10,40 @@
     #          endfunction
     #        '';
   };
+
+  keymaps = [
+    # Markdown preview controls
+    {
+      mode = "n";
+      key = "<leader>ms";
+      action = ":MarkdownPreviewStop<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>mp";
+      action = ":MarkdownPreview<CR>";
+    }
+    # Enable/Disable wordwrap
+    {
+      key = "<leader>we";
+      action = ":set wrap<CR>";
+      mode = "n";
+    }
+    {
+      key = "<leader>ws";
+      action = ":set nowrap<CR>";
+      mode = "n";
+    }
+    # Change spelling
+    {
+      key = "<leader>ls";
+      action = ":set spell spelllang=es_gt<CR>";
+      mode = "n";
+    }
+    {
+      key = "<leader>le";
+      action = ":set spell spelllang=en_us<CR>";
+      mode = "n";
+    }
+  ];
 }
