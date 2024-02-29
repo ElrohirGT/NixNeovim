da7dd8fe3442e9d03211f219011d466daed2392b
ElrohirGT <elrohirgt@gmail.com>
2024-02-29 13:58:45 -0600

    feat: added a mappings file

--- config/default.nix
+++ config/default.nix
@@ -11,6 +11,7 @@
 		./markdown_preview.nix
 		./language_servers.nix
 		./error_panes.nix
+		./mappings.nix
   ];
 
   config = {
@@ -77,118 +78,6 @@
       # Support for surrounding things with things
       surround.enable = true;
     };
-
-    keymaps = [
-      # Append previous line to current line
-      {
-        mode = "v";
-        key = "J";
-        action = ":m '>+1<CR>gv=gv";
-      }
-      {
-        mode = "v";
-        key = "K";
-        action = ":m '<-2<CR>gv=gv";
-      }
-      {
-        mode = "n";
-        key = "J";
-        action = "mzJ`z";
-      }
-
-      # Toggle NvimTree
-      {
-        mode = "n";
-        key = "<leader>e";
-        action = ":NvimTreeToggle<CR>";
-      }
-
-      # CTRL-q saves and closes file
-      {
-        mode = "n";
-        key = "<C-Q>";
-        action = ":wq<CR>";
-      }
-
-      # :w and :q are case insensitive
-      {
-        mode = "n";
-        key = ":W";
-        action = ":w";
-      }
-      {
-        mode = "n";
-        key = ":Q";
-        action = ":q<CR>";
-      }
-
-      # CTRL-d/CTRL-u now centers the screen at cursor
-      {
-        mode = "n";
-        key = "<C-d>";
-        action = "<C-d>zz";
-      }
-      {
-        mode = "n";
-        key = "<C-u>";
-        action = "<C-u>zz";
-      }
-
-      # Next/Previous in search centers the screen
-      {
-        mode = "n";
-        key = "n";
-        action = "nzzzv";
-      }
-      {
-        mode = "n";
-        key = "N";
-        action = "Nzzzv";
-      }
-      {
-        mode = "x";
-        key = "<leader>p";
-        action = "\"_dP";
-      }
-
-      # Yank to system clipboard
-      {
-        mode = "n";
-        key = "<leader>y";
-        action = "\"+y";
-      }
-      {
-        mode = "v";
-        key = "<leader>y";
-        action = "\"+y";
-      }
-      {
-        mode = "n";
-        key = "<leader>Y";
-        action = "\"+Y";
-      }
-
-      # Disable Q
-      {
-        mode = "n";
-        key = "Q";
-        action = "<nop>";
-      }
-
-      # Rename symbol using IncRename
-      {
-        mode = "n";
-        key = "<leader>rn";
-        action = ":IncRename";
-      }
-
-      # Vim fugitive
-      {
-        mode = "n";
-        key = "<leader>gl";
-        action = ":Git log --oneline --graph<CR>";
-      }
-    ];
 
     extraPlugins = with pkgs; [
       ripgrep # For Telescope
---
+++ config/mappings.nix
@@ -0,0 +1,113 @@
+{
+  keymaps = [
+    # Append previous line to current line
+    {
+      mode = "v";
+      key = "J";
+      action = ":m '>+1<CR>gv=gv";
+    }
+    {
+      mode = "v";
+      key = "K";
+      action = ":m '<-2<CR>gv=gv";
+    }
+    {
+      mode = "n";
+      key = "J";
+      action = "mzJ`z";
+    }
+
+    # Toggle NvimTree
+    {
+      mode = "n";
+      key = "<leader>e";
+      action = ":NvimTreeToggle<CR>";
+    }
+
+    # CTRL-q saves and closes file
+    {
+      mode = "n";
+      key = "<C-Q>";
+      action = ":wq<CR>";
+    }
+
+    # :w and :q are case insensitive
+    {
+      mode = "n";
+      key = ":W";
+      action = ":w";
+    }
+    {
+      mode = "n";
+      key = ":Q";
+      action = ":q<CR>";
+    }
+
+    # CTRL-d/CTRL-u now centers the screen at cursor
+    {
+      mode = "n";
+      key = "<C-d>";
+      action = "<C-d>zz";
+    }
+    {
+      mode = "n";
+      key = "<C-u>";
+      action = "<C-u>zz";
+    }
+
+    # Next/Previous in search centers the screen
+    {
+      mode = "n";
+      key = "n";
+      action = "nzzzv";
+    }
+    {
+      mode = "n";
+      key = "N";
+      action = "Nzzzv";
+    }
+    {
+      mode = "x";
+      key = "<leader>p";
+      action = "\"_dP";
+    }
+
+    # Yank to system clipboard
+    {
+      mode = "n";
+      key = "<leader>y";
+      action = "\"+y";
+    }
+    {
+      mode = "v";
+      key = "<leader>y";
+      action = "\"+y";
+    }
+    {
+      mode = "n";
+      key = "<leader>Y";
+      action = "\"+Y";
+    }
+
+    # Disable Q
+    {
+      mode = "n";
+      key = "Q";
+      action = "<nop>";
+    }
+
+    # Rename symbol using IncRename
+    {
+      mode = "n";
+      key = "<leader>rn";
+      action = ":IncRename";
+    }
+
+    # Vim fugitive
+    {
+      mode = "n";
+      key = "<leader>gl";
+      action = ":Git log --oneline --graph<CR>";
+    }
+  ];
+}
