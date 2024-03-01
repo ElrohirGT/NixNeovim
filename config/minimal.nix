4539712407d34a8fea702bc48363375756190847
ElrohirGT <elrohirgt@gmail.com>
2024-02-29 20:13:49 -0600

    feat: removed some modules from minimal

--- config/minimal.nix
+++ config/minimal.nix
@@ -2,9 +2,7 @@
   # Import all your configuration modules here
   imports = [
     ./general.nix
-    ./modules/telescope.nix
     ./modules/completions.nix
-    ./modules/treesitter.nix
     ./modules/language_servers.nix
     ./modules/mappings.nix
   ];
