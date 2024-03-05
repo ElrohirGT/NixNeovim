949468b19e8eb3b4243f471b90f6010458dd17f1
ElrohirGT <elrohirgt@gmail.com>
2024-03-05 10:24:40 -0600

    feat: Added recursive import for .nix files

--- config/default.nix
+++ config/default.nix
@@ -1,15 +1,31 @@
-{
+{lib, ...}:
+with lib; let
+  # Recursively constructs an attrset of a given folder, recursing on directories, value of attrs is the filetype
+  getDir = dir:
+    mapAttrs
+    (
+      file: type:
+        if type == "directory"
+        then getDir "${dir}/${file}"
+        else type
+    )
+    (builtins.readDir dir);
+
+  # Collects all files of a directory as a list of strings of paths
+  files = dir: collect isString (mapAttrsRecursive (path: type: concatStringsSep "/" path) (getDir dir));
+
+  # Filters out directories that don't end with .nix or are this file, also makes the strings absolute
+  validFiles = dir:
+    map
+    (file: ./. + "/${file}")
+    (filter
+      (file: hasSuffix ".nix" file && file != "default.nix")
+      (files dir));
+in {
   # Import all your configuration modules here
-  imports = [
-    ./general.nix
-    ./modules/telescope.nix
-    ./modules/completions.nix
-    ./modules/treesitter.nix
-    ./modules/markdown_preview.nix
-    ./modules/language_servers.nix
-    ./modules/error_panes.nix
-    ./modules/mappings.nix
-    ./modules/snippets.nix
-		./modules/formatting.nix
-  ];
+  imports =
+    [
+      ./general.nix
+    ]
+    ++ validFiles ./modules;
 }
