836412cab80d957973caaa5b0f3dec76abda2cf5
ElrohirGT <elrohirgt@gmail.com>
2024-03-07 16:25:28 -0600

    fix: Added extra formatters

--- config/modules/formatting.nix
+++ config/modules/formatting.nix
@@ -7,12 +7,23 @@
     };
     formatters = {
       sqlfluff = {
-        "inherit" = false;
+        "inherit" = false; # Don't merge with default config
         command = "${pkgs.sqlfluff}/bin/sqlfluff";
         # Since `sqlfluff` formats the file on disk by default
         # and it doesn't support outputing the formatted file to stdout
         # we need to add the `cat` command to print to stdout
         args = ["format" "--dialect" "postgres" "$FILENAME" "&&" "cat" "$FILENAME"];
+      };
+      alejandra = {
+        "inherit" = false;
+        command = "${pkgs.alejandra}/bin/alejandra";
+        args = ["$FILENAME"];
+      };
+      shfmt = {
+        command = "${pkgs.shfmt}/bin/shfmt";
+      };
+      prettierd = {
+        command = "${pkgs.prettierd}/bin/prettierd";
       };
     };
 
